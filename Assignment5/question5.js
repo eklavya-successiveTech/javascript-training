dec03e86e2e799e038ed1d1d2facf74bcd594f5d
eklavya-successiveTech <eklavya.sanmotra@successive.tech>
2025-06-15 15:04:40 +0530

    Assignment5 complete

--- Assignment5/question5.js
+++ Assignment5/question5.js
@@ -5,24 +5,40 @@
 // };
 // Output: { 'a': 1, 'b.c': 2, 'b.d.0': 3, 'b.d.1': 4 }
 
-const result = {};
-const flattenObject = (obj)=>{
-    for( let properties in obj){
-        if(typeof obj[properties] == "object"){
-            flattenObject(obj[properties]); 
-        }
-        result[properties] = obj[properties];
+const flattenObject = (obj) => {
+  const flattened = {};
+  const process = (currentPart, path) => {
+    if (typeof currentPart === 'object' && currentPart !== null) {
+      for (const key in currentPart) {
+        const newPath = path ? `${path}.${key}` : key;
+        process(currentPart[key], newPath);
+      }
+    } else {
+      flattened[path] = currentPart;
     }
-    return result;
-}
+  };
+  process(obj, '');
+  return flattened;
+};
+
+const obj = {
+   a: 1,
+   b: { c: 2, d: [3, 4] }
+};
+
+const result = flattenObject(obj);
+console.log(result);
 
 const employee = {
-    name : "Eklavya",
-    age : 21,
-    home : "Udhampur",
-    team : {
-        1 : "Client1",
-        2 : "Client2",
-        3 : "Client3"
+    name: "Eklavya",
+    age: 21,
+    home: "Udhampur",
+    team: {
+        1: "Client1",
+        2: "Client2",
+        3: "Client3"
     }
-}
+};
+
+const flatEmployee = flattenObject(employee);
+console.log(flatEmployee);
