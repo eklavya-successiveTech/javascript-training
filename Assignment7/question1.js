a00b1f76d6eebd3c1d41a3dea9ce4d5400e0444a
eklavya-successiveTech <eklavya.sanmotra@successive.tech>
2025-06-23 09:47:16 +0530

    Assignment7 complete

--- Assignment7/question1.js
+++ Assignment7/question1.js
@@ -1,28 +1,30 @@
 // 1. Give an example of using a callback function to handle an asynchronous operation in JavaScript (Use any open api to make a call)
 
 function fetchData(callback) {
-    fetch('https://api.sampleapis.com/coffee/hot')
-        .then(response => {
-            if (!response.ok) {
-                callback(`Error: ${response.status}`, null);
-            } else {
-                return response.json();
-            }
-        })
-        .then(data => {
-            callback(null, data);
-        })
-        .catch(error => {
-            callback(error, null);
-        });
-}
+  async function getData() {
+    try {
+      const response = await fetch('https://api.sampleapis.com/coffee/hot');
 
-function callback(err, data) {
-    if (err) {
-        console.error('Callback Error:', err);
-    } else {
-        console.log('Callback Data:', data);
+      if (!response.ok) {
+        return callback(`HTTP Error: ${response.status}`, null);
+      }
+
+      const data = await response.json();
+      callback(null, data);
+    } catch (error) {
+      callback(error.message, null);
     }
+  }
+
+  getData();
 }
 
-fetchData(callback);
+function handleResult(err, data) {
+  if (err) {
+    console.error('Callback Error:', err);
+  } else {
+    console.log('Callback Data:', data);
+  }
+}
+
+fetchData(handleResult);
