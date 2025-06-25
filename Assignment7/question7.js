2b097d80439409eb6547424075f92f8b28473640
Eklavya Sanmotra <eklavya.sanmotra@eklavyavrs-28.successivetech.com>
2025-06-25 13:02:14 +0530

    Assignment7 update

--- Assignment7/question7.js
+++ Assignment7/question7.js
@@ -12,15 +12,8 @@ const p3 = new Promise((resolve)=>{
     setTimeout(()=> resolve("P3 resolved "), 2000)
 })
 
-async function multiAsyncOperations(p1,p2,p3) {
-    const r1Promise = p1;
-    const r2Promise = p2;
-    const r3Promise = p3;
-    
-    const r1 = await r1Promise;
-    const r2 = await r2Promise;
-    const r3 = await r3Promise;
-    
-    return [r1,r2,r3]
+function multiAsyncOperations(p1,p2,p3) {
+     const result = Promise.all([p1,p2,p3]);
+    return result;
 } 
 multiAsyncOperations(p1,p2,p3).then((data)=> console.log(data));
